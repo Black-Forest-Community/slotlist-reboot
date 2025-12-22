96c075fe0608703561b5edef1afa1597dbf3a4f1
Niklas Merz <niklas@merz.dev>
2025-12-22 21:30:53 +0100

    change header

--- frontend/src/i18n/views/Home.de.vue
+++ frontend/src/i18n/views/Home.de.vue
@@ -1,5 +1,5 @@
 <template>
   <b-jumbotron header="Slotlist Reboot Project" lead="In Arbeit...">
-    Der Nachfolger von slotlist.info? Wir haben schonmal alle Missionen importiert.
+    Der Nachfolger von slotlist.info ganz nah Original. Open Source Rules!
   </b-jumbotron>
 </template>
